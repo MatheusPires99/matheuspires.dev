ac1da40a64e7877245439038a2f52caa3f1c80f2
Matheus Pires <matheushenriquepires99@gmail.com>
2022-09-30 13:44:07 -0300

    chore: Setup ESLint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
-}
+  return <Component {...pageProps} />;
+};
 
-export default App
+export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 const Home = () => {
-  return (
-    <h1>Hello World</h1>
-  )
-}
+  return <h1>Hello World</h1>;
+};
 
-export default Home
+export default Home;
