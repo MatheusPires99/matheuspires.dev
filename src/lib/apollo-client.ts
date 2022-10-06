d7622b895b51a2474761c832d1c7b4d3a243dc29
Matheus Pires <matheushenriquepires99@gmail.com>
2022-10-06 20:41:28 -0300

    chore: Add AUTH_TOKEN env variable and rename API_URL

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: process.env.CMS_API_URL,
+  uri: process.env.HYGRAPH_API_URL,
   cache: new InMemoryCache(),
+  headers: {
+    Authorization: `Bearer ${process.env.HYGRAPH_AUTH_TOKEN}`,
+  },
 });
