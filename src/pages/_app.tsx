c329a0b89867959a686a6873353d6347f2e2eb5b
Matheus Pires <matheushenriquepires99@gmail.com>
2023-05-02 21:09:54 -0300

    chore: Update to Nextjs latest version and create css variables

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
+import { Inter, Fira_Mono } from "next/font/google";
 
 import "@/styles/global.css";
 
@@ -7,26 +8,46 @@ import { Container } from "@/components/container";
 import { Footer } from "@/components/footer";
 import { GetInTouch } from "@/components/get-in-touch";
 import { Header } from "@/components/header";
-import { lightTheme } from "@/styles";
 import { globalStyles } from "@/styles/global";
 
 globalStyles();
 
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+  display: "swap",
+});
+
+const firaMono = Fira_Mono({
+  subsets: ["latin"],
+  variable: "--font-code",
+  weight: ["400", "500"],
+  display: "swap",
+});
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider
-      disableTransitionOnChange
-      attribute="class"
-      value={{ dark: "dark-theme", light: lightTheme.className }}
-      defaultTheme="dark"
-    >
-      <Header />
-      <Container as="main" css={{ flex: 1, marginTop: "$sizes$header-height" }}>
-        <Component {...pageProps} />
-      </Container>
-      <GetInTouch />
-      <Footer />
-    </ThemeProvider>
+    <>
+      {/* eslint-disable-next-line react/no-unknown-property */}
+      <style jsx global>{`
+        :root {
+          --font-sans: ${inter.style.fontFamily};
+          --font-code: ${firaMono.style.fontFamily};
+        }
+      `}</style>
+
+      <ThemeProvider attribute="class" enableSystem defaultTheme="system">
+        <Header />
+        <Container
+          as="main"
+          css={{ flex: 1, marginTop: "$sizes$header-height" }}
+        >
+          <Component {...pageProps} />
+        </Container>
+        <GetInTouch />
+        <Footer />
+      </ThemeProvider>
+    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "var(--background)",
+        "background-light": "var(--background-light)",
+        shape: "var(--shape)",
+        "shape-hover": "var(--shape-hover)",
+        border: "var(--border)",
+        "border-hover": "var(--border-hover)",
+        "solid-background": "var(--solid-background)",
+        "solid-background-hover": "var(--solid-background-hover)",
+        "text-contrast": "var(--text-contrast)",
+        "text-base": "var(--text-base)",
+
+        "accent-border": "var(--accent-border)",
+        "accent-placeholder": "var(--accent-placeholder)",
+        "accent-text": "var(--accent-text)",
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+        code: ["var(--font-code)", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
