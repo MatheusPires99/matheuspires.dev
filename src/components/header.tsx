56594543d5a8bf5281424dd395d8b9b0236fdd0a
Matheus Pires <matheushenriquepires99@gmail.com>
2023-05-03 15:14:35 -0300

    style: Update header background color

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -25,7 +25,7 @@ export const Header = () => {
   }, []);
 
   return (
-    <header className="bg-background/75 fixed top-0 z-50 h-20 w-full border-b border-accent-border backdrop-blur-md">
+    <header className="fixed top-0 z-50 h-20 w-full border-b border-accent-border bg-[rgba(253,253,254,0.75)] backdrop-blur-md dark:bg-[rgba(19,22,32,0.75)]">
       <div className="container flex h-full items-center justify-between">
         <Link
           href="/"
