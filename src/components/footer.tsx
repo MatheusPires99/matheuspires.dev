a121600635c8225fb404a63d98414b169707b2d5
Matheus Pires <matheushenriquepires99@gmail.com>
2023-05-02 21:26:17 -0300

    chore: Migrate Footer component to TailwindCSS

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,16 +4,15 @@ import { IconButton } from "@/components/icon-button";
 import { GithubLogo } from "@/components/icons";
 import { Link } from "@/components/link";
 
-import { Section } from "../section";
-import { ContactInfo, DesignedBy } from "./styles";
+import { Section } from "./section";
 
 export const Footer = () => {
   return (
     <Section as="footer" css={{ paddingTop: 0 }}>
       <div className="container flex flex-col items-center">
-        <ContactInfo>
-          <strong>© Matheus Pires 2022</strong>
-          <div>
+        <div className="flex items-center gap-10">
+          <strong className="text-lg">© Matheus Pires 2022</strong>
+          <div className="flex items-center">
             <IconButton
               as={Link}
               href="https://www.linkedin.com/in/matheus-pires-521271180/"
@@ -31,10 +30,10 @@ export const Footer = () => {
               <GithubLogo />
             </IconButton>
           </div>
-        </ContactInfo>
+        </div>
 
-        <DesignedBy>
-          Designed by{" "}
+        <div className="mt-4">
+          <span className="text-accent-text">Designed by</span>{" "}
           <Link
             variant="hightlight"
             href="https://www.linkedin.com/in/gabriela-rezende-61a7881ba/"
@@ -42,7 +41,7 @@ export const Footer = () => {
           >
             Gabriela Rezende
           </Link>
-        </DesignedBy>
+        </div>
       </div>
     </Section>
   );
