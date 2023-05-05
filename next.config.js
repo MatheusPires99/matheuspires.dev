43271feda1b046966c45e6f49602ff6d9d27cb62
Matheus Pires <matheushenriquepires99@gmail.com>
2023-05-04 21:22:21 -0300

    chore: Add headers config in next.config.js

--- next.config.js
+++ next.config.js
@@ -9,6 +9,27 @@ const nextConfig = withPlaiceholder({
   experimental: {
     appDir: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/favicons/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://www.matheuspires.dev",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, OPTIONS, PUT, DELETE",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Origin, X-Requested-With, Content-Type, Accept",
+          },
+        ],
+      },
+    ];
+  },
 });
 
 module.exports = nextConfig;
